8561132450312bbbe9103d5ff04c03ae064c6c40
Robert J. Paruch <roobbb@live.com>
2019-06-16 02:09:52 -0700

    Added the POST API end-point

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,4 +14,53 @@ module.exports = function(app) {
         res.json(err);
       });
   });
+
+  // Add an inventory item.
+  // Example of the body of the request:
+  // {
+  //   name: 'milk',
+  //   expireDate: '2019-06-20T01:48:21.458Z',
+  //   qty: 1,
+  //   unit: 'gal',
+  //   storePlace: [ { name: 'refrigerator' } ],
+  //   dateIn: '2019-06-14T01:48:21.458Z'
+  // }
+  app.post('/api/inventory', function(req, res) {
+    db.storages
+      .findOneAndUpdate(
+        { name: req.body.storePlace[0].name },
+        { $set: { name: req.body.storePlace[0].name } },
+        { upsert: true, new: true }
+      )
+      .then(function(storage) {
+        return db.inventory.findOneAndUpdate(
+          {
+            name: req.body.name,
+            expireDate: req.body.expireDate
+          },
+          {
+            $set: {
+              name: req.body.name,
+              expireDate: req.body.expireDate,
+              qty: req.body.qty,
+              unit: req.body.unit,
+              dateIn: req.body.dateIn
+            },
+            $push: {
+              storePlace: storage._id
+            }
+          },
+          {
+            upsert: true,
+            new: true
+          }
+        );
+      })
+      .then(function(item) {
+        res.json(item);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
 };
