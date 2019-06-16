b68b215b17711d1986fefc4f2a4da39f0fac3868
Robert J. Paruch <roobbb@live.com>
2019-06-16 02:10:16 -0700

    Added the PUT API end-point

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -63,4 +63,42 @@ module.exports = function(app) {
         res.json(err);
       });
   });
+
+  // Update an inventory item
+  app.put('/api/inventory/:item_id', function(req, res) {
+    db.storages
+      .findOneAndUpdate(
+        { name: req.body.storePlace[0].name },
+        { $set: { name: req.body.storePlace[0].name } },
+        { upsert: true, new: true }
+      )
+      .then(function(storage) {
+        return db.inventory.findOneAndUpdate(
+          {
+            _id: req.params.item_id
+          },
+          {
+            $set: {
+              name: req.body.name,
+              expireDate: req.body.expireDate,
+              qty: req.body.qty,
+              unit: req.body.unit,
+              dateIn: req.body.dateIn
+            },
+            $push: {
+              storePlace: storage._id
+            }
+          },
+          {
+            new: true
+          }
+        );
+      })
+      .then(function(item) {
+        res.json(item);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
 };
