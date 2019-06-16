764f9dc8ffb5b8868311266df682a01d0f75bbc1
Robert J. Paruch <roobbb@live.com>
2019-06-16 02:09:15 -0700

    Added the GET API end-point

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,4 +2,16 @@ const db = require('../models');
 
 // API routes
 module.exports = function(app) {
+  // Get the inventory
+  app.get('/api/inventory', function(req, res) {
+    db.inventory
+      .find({})
+      .populate('storePlace')
+      .then(function(inventory) {
+        res.json(inventory);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
 };
