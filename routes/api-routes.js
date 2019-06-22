53627f000d447d495b1ba8b43cfc1418e1989021
Robert J. Paruch <roobbb@live.com>
2019-06-22 13:09:41 -0700

    Corrected

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,5 @@
 const axios = require('axios');
 const db = require('../models');
-const axios = require("axios")
-const router = require("express").Router();
-
-router.get("/recipes", (req, res) => {
-  axios
-    .get("http://www.recipepuppy.com/api/", { params: req.query })
-    .then(({ data: { results } }) => res.json(results))
-    .catch(err => res.status(422).json(err));
-});
 
 // API routes
 module.exports = function(app) {
