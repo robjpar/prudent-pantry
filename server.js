b1dee913f620276558fab85b11698d466bb8adc7
Mohsensho <shojaeifar.mo@yahoo.com>
2019-06-27 18:46:28 -0700

    production route setup

--- server.js
+++ server.js
@@ -38,15 +38,7 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Auth Route
 app.use("/api/users", users);
-// server static assets if in production
-if(process.env.NODE_ENV === 'production')
-{
-  // set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  }); 
-}
+
 
 // Routing
 require("./routes/api-routes.js")(app);
