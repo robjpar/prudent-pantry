bc3ed796e419b1eea87a69ffec0d56efffbc6c29
Robert J. Paruch <roobbb@live.com>
2019-06-29 01:23:32 -0700

    Moved the connection to a separate file

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 
-// Link to mLab MongoDB (Heroku) or local MongoDB
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/prudent_pantry_db";
-
-// Connect to the Mongo DB
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true
-});
+// Connect to MongoDB
+require('./config/connection');
 
 // Server configuration
 const app = express();
