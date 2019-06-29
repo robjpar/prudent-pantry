2d33c950a40b2aa7e4023975c0b6eee4f13b01b0
Robert J. Paruch <roobbb@live.com>
2019-06-29 02:34:07 -0700

    The db name corrected

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Link to mLab MongoDB (Heroku) or local MongoDB
 const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost/great_books_db';
+  process.env.MONGODB_URI || 'mongodb://localhost/prudent_pantry_db';
 
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, {
