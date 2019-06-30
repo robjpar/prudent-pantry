01e0b804b8ac05946f716895d6339f886573362e
Robert J. Paruch <roobbb@live.com>
2019-06-30 01:55:33 -0700

    Added the front-end support for user id

--- client/src/utils/api-routes.js
+++ client/src/utils/api-routes.js
@@ -2,18 +2,16 @@ import axios from "axios";
 
 
 export default {
-  // Gets all inventory
-  getInventory: function() {
-    return axios.get("/api/inventory");
+  // Gets all inventory for given user
+  getInventory: function(userId) {
+    return axios.get("/api/inventory/" + userId);
   },
-  // // Gets the food with the given id
-  // getfood: function(id) {
-  //   return axios.get("/api/foods/" + id);
-  // },
+  
   // Deletes the food with the given id
   deleteFood: function(id) {
     return axios.delete("/api/inventory/" + id);
   },
+
   // Saves a food to the database
   saveItem: function(itemData) {
     console.log(itemData);
